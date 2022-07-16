a5aaec8e50da1a83a51aeb3b2a0bdc67243b60e7
Victor Lelis <ovictorlelis@gmail.com>
2022-07-15 21:36:34 -0300

    aula03

--- app/router/router.php
+++ app/router/router.php
@@ -8,10 +8,22 @@ function routes()
 function exactMatchUriInArrayRoutes($uri, $routes)
 {
   if (array_key_exists($uri, $routes)) {
-    return [];
+    return [$uri => $routes[$uri]];
   }
 
   return [];
+}
+
+function regularExpressionMatchArrayRoutes($uri, $routes)
+{
+  return array_filter(
+    $routes,
+    function ($value) use ($uri) {
+      $regex = str_replace('/', '\/', ltrim($value, '/'));
+      return preg_match("/^$regex$/", ltrim($uri, '/'));
+    },
+    ARRAY_FILTER_USE_KEY
+  );
 }
 
 function router()
@@ -19,4 +31,10 @@ function router()
   $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
   $routes = routes();
   $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
+
+  if (empty($matchedUri)) {
+    $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+  }
+
+  var_dump($matchedUri);
 }
--- app/router/routes.php
+++ app/router/routes.php
@@ -1,5 +1,6 @@
 <?php
 return [
   '/' => 'Home@index',
-  '/user/create' => 'User@create'
+  '/user/create' => 'User@create',
+  '/user/[a-z0-9]+' => "User@create"
 ];
